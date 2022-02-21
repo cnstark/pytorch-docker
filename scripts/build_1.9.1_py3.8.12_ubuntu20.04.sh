7a91e42db144531533c48f2740632d552026ed75
Yuhao Wang <yuhaow97@gmail.com>
2022-02-22 00:00:14 +0800

    feature: 1.9.1_py3.8.12_ubuntu20.04

---
+++ scripts/build_1.9.1_py3.8.12_ubuntu20.04.sh
@@ -0,0 +1,7 @@
+export UBUNTU_VERSION=20.04
+export PYTHON_VERSION=3.8.12
+export PYTORCH_VERSION=1.9.1
+export TORCHVISION_VERSION=0.10.1
+export TORCHAUDIO_VERSION=0.9.1
+
+./docker/ubuntu/build.sh
