11e981be38bbf4fd4a617eaaf702595438d1a91f
Yuhao Wang <yuhaow97@gmail.com>
2022-02-24 14:14:39 +0800

    feature: 1.7.0_py3.9.10_[cuda11.0_]ubuntu18.04

---
+++ scripts/build_1.7.0_py3.9.10_ubuntu18.04.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export UBUNTU_VERSION=18.04
+
+export PYTHON_VERSION=3.9.10
+
+export PYTORCH_VERSION=1.7.0
+export PYTORCH_VERSION_SUFFIX=+cpu
+export TORCHVISION_VERSION=0.8.0
+export TORCHVISION_VERSION_SUFFIX=+cpu
+export TORCHAUDIO_VERSION=0.7.0
+export TORCHAUDIO_VERSION_SUFFIX=
+export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/torch_stable.html
+
+./docker/ubuntu/build.sh
