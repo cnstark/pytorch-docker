503621d8a6f8bef2c06e146eb91172ed047c54e1
Yuhao Wang <yuhaow97@gmail.com>
2023-02-01 13:58:55 +0800

    feature: 1.13.1_py3.9.16_cuda11.7[_devel]_ubuntu22.04

---
+++ scripts/build_1.13.1_py3.9.16_ubuntu20.04.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export BASE_IMAGE=ubuntu:20.04
+
+export PYTHON_VERSION=3.9.16
+
+export PYTORCH_VERSION=1.13.1
+export PYTORCH_VERSION_SUFFIX=+cpu
+export TORCHVISION_VERSION=0.14.1
+export TORCHVISION_VERSION_SUFFIX=+cpu
+export TORCHAUDIO_VERSION=0.13.1
+export TORCHAUDIO_VERSION_SUFFIX=+cpu
+export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/cpu/torch_stable.html
+
+export IMAGE_TAG=1.13.1-py3.9.16-ubuntu20.04
+
+./docker/ubuntu/build.sh
