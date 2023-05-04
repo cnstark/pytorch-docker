e9c195cee03094edb18ae3645c8f0c96a32b6f0d
Julian Gaal <22290570+juliangaal@users.noreply.github.com>
2023-05-04 03:26:21 +0000

    [WIP] Initial support for Pytorch 2.0, Ubuntu 22/20.04, Cuda 11.8/7 Python 3.9 (#8)
    * initial pytorch 2.0 support
    
    * match pytorch version with pytorchvision/pytorchaudio
    
    * fix table formatting for pytorch2.0.0 versions
    
    ---------
    
    Co-authored-by: Julian Gaal <julian.gaal@anavs.de>

---
+++ scripts/build_2.0.0_py3.9.12_cuda11.7.1_ubuntu22.04.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export BASE_IMAGE=ubuntu:22.04
+
+export PYTHON_VERSION=3.9.12
+
+export PYTORCH_VERSION=2.0.0
+export PYTORCH_VERSION_SUFFIX=+cu117
+export TORCHVISION_VERSION=0.15.0
+export TORCHVISION_VERSION_SUFFIX=+cu117
+export TORCHAUDIO_VERSION=2.0.0
+export TORCHAUDIO_VERSION_SUFFIX=+cu117
+export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/cu117/torch_stable.html
+
+export IMAGE_TAG=2.0.0-py3.9.12-cuda11.7.1-ubuntu22.04
+
+./docker/ubuntu/build.sh
