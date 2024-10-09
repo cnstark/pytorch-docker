c6b837f45a3fc576c755ae9e2ee392e8f7f08a5b
Yuhao Wang <yuhaow97@gmail.com>
2024-10-10 01:48:10 +0800

    feature: 2.1.2_py3.10.15_ubuntu22.04

---
+++ scripts/build_2.1.2_py3.10.15_cuda12.1.0_ubuntu22.04.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export BASE_IMAGE=ubuntu:22.04
+
+export PYTHON_VERSION=3.10.15
+
+export PYTORCH_VERSION=2.1.2
+export PYTORCH_VERSION_SUFFIX=+cu121
+export TORCHVISION_VERSION=0.16.2
+export TORCHVISION_VERSION_SUFFIX=+cu121
+export TORCHAUDIO_VERSION=2.1.2
+export TORCHAUDIO_VERSION_SUFFIX=+cu121
+export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/cu121/torch_stable.html
+
+export IMAGE_TAG=2.1.2-py3.10.15-cuda12.1.0-ubuntu22.04
+
+./docker/ubuntu/build.sh
