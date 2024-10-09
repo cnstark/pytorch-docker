62a1e2b4aeff359cfbbe8ee030e40ef7024ea820
Yuhao Wang <yuhaow97@gmail.com>
2024-10-10 01:58:10 +0800

    feature: 2.3.1_py3.10.15_ubuntu22.04

---
+++ scripts/build_2.3.1_py3.10.15_ubuntu22.04.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export BASE_IMAGE=ubuntu:22.04
+
+export PYTHON_VERSION=3.10.15
+
+export PYTORCH_VERSION=2.3.1
+export PYTORCH_VERSION_SUFFIX=+cpu
+export TORCHVISION_VERSION=0.18.1
+export TORCHVISION_VERSION_SUFFIX=+cpu
+export TORCHAUDIO_VERSION=2.3.1
+export TORCHAUDIO_VERSION_SUFFIX=+cpu
+export PYTORCH_DOWNLOAD_URL=https://download.pytorch.org/whl/cpu/torch_stable.html
+
+export IMAGE_TAG=2.3.1-py3.10.15-ubuntu22.04
+
+./docker/ubuntu/build.sh
